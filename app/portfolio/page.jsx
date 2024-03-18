fee45d919e49750998b7d242114395514ac9c1ca
Thai Bao <baont0209@gmail.com>
2024-03-18 10:23:27 +0700

    finally

--- app/portfolio/page.jsx
+++ app/portfolio/page.jsx
@@ -54,20 +54,64 @@ const PorfolioPage = () => {
       transition={{ duration: 1 }}
     >
       <div className="h-[600vh] relative" ref={ref}>
-        <div className="w-screen h-[calc(100vh-6rem] flex items-center justify-center text-8xl text-center"></div>
-        <div className="sticky top-0 h-screnn gap-4 items-center">
-          {items.map((item) => (
-            <div className="" key={item.id}>
-              <div className="">
-                <h1>{item.title}</h1>
-                <div cla0ssName="relative">
-                  <Image src={item.img} alt="" fill />
+        <div className="w-screen h-[calc(100vh-6rem] flex items-center justify-center text-8xl text-center">
+          My Works
+        </div>
+        <div className="sticky top-0 h-screen gap-4 items-center overflow-hidden">
+          <motion.div>
+            {items.map((item) => (
+              <div
+                className={`h-screen w-screen flex items-center justify-center bg-gradient-to-r ${item.color}`}
+                key={item.id}
+              >
+                <div className="flex flex-col gap-8 text-white">
+                  <h1 className="text-xl font-bold md:text-4xl lg:text-6xl xl:text-8xl">
+                    {item.title}
+                  </h1>
+                  <div className="relative w-80 h-56 md:w-96 md:h-64 lg:w-[500px] lg:h-[350px] xl:w-[600px] xl:h-[420px]">
+                    <Image src={item.img} alt="" fill />
+                  </div>
+                  <p className="w-80 md:w-96 lg:w-[500px] lg:text-lg xl:w-[600px]">
+                    {item.desc}
+                  </p>
+                  <Link href={item.link} className="flex justify-end">
+                    <button className="p-2 text-sm md:p-4 md:text-md lg:p-8 lg:text-lg bg-white text-gray-600 font-semibold m-4 rounded">
+                      See Demo
+                    </button>
+                  </Link>
                 </div>
-                <p>{item.desc}</p>
-                <Link href={item.link}>See Demo</Link>
               </div>
-            </div>
-          ))}
+            ))}
+          </motion.div>
+        </div>
+      </div>
+      <div className="w-screen h-screen flex flex-col gap-16 items-center justify-center text-center">
+        <h1 className="text-8xl">Do you have a project?</h1>
+        <div className="relative">
+          <motion.div
+            animate={{ rotate: 360 }}
+            transition={{ duration: 8, ease: "linear", repeat: Infinity }}
+            viewBox="0 0 300 300"
+            className="w-64  h-64  md:w-[500px] md:h-[500px]"
+          >
+            <defs>
+              <path
+                id="circlePath"
+                d="M 150, 150 m -60, 0 a 60,60 0 0,1 120,0 a 60,60 0 0,1 -120,0 "
+              />
+            </defs>
+            <text>
+              <textPath xlinkHref="#circlePath" className="text-xl">
+                Front-end Developer and UI Designer
+              </textPath>
+            </text>
+          </motion.div>
+          <Link
+            href="/contract"
+            className="w-16 h-16 md:w-28 md:h-28 absolute top-0 left-0 right-0 bottom-0 m-auto bg-black text-white  rounded-full flex items-center justify-center"
+          >
+            Hire Me
+          </Link>
         </div>
       </div>
     </motion.div>
